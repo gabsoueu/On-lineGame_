f0361acc5e316eb16ff96acabf53f44262790772
ProfaTatianeTobias <93044628+ProfaTatianeTobias@users.noreply.github.com>
2022-07-07 16:31:22 -0300

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -27,10 +27,24 @@ class Game {
     fuels = new Group();
     coins = new Group();
 
+    var obstaclesPositions = [
+      { x: width / 2 + 250, y: height - 800, image: obstacle2Image },
+      { x: width / 2 - 150, y: height - 1300, image: obstacle1Image },
+      { x: width / 2 + 250, y: height - 1800, image: obstacle1Image },
+      { x: width / 2 - 180, y: height - 2300, image: obstacle2Image },
+      { x: width / 2, y: height - 2800, image: obstacle2Image },
+      { x: width / 2 - 180, y: height - 3300, image: obstacle1Image },
+      { x: width / 2 + 180, y: height - 3300, image: obstacle2Image },
+      { x: width / 2 + 250, y: height - 3800, image: obstacle2Image },
+      { x: width / 2 - 150, y: height - 4300, image: obstacle1Image },
+      { x: width / 2 + 250, y: height - 4800, image: obstacle2Image },
+      { x: width / 2, y: height - 5300, image: obstacle1Image },
+      { x: width / 2 - 180, y: height - 5500, image: obstacle2Image }
+    ];
+    
     this.addSprites(fuels,1,fuelImg,0.02);
 
     this.addSprites(coins,10,coinImg,0.09);
-
 
   }
 
